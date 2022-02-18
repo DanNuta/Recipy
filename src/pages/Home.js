739e168a181fa86d9fd46cb149124f383fc06567
DanNuta <dany.condurari@gmail.com>
2022-02-18 18:57:33 +0200

    Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
             {isLoading && <h1>Loading...</h1>}
             {error && <h1>{error}</h1>}
             {data.map(item => (
-                <h1>{item.title}</h1>
+                <Card/>
             ))}
         </div>
      );
